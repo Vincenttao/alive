bc2ae2f47ba2824115370810eb45fbdc3e1d86cd
vincenttao <13512895+vinctao@user.noreply.gitee.com>
2023-11-11 10:03:29 +0800

    Commit

---
+++ app/src/main/java/com/alivehealth/alive/motiondetect.kt
@@ -0,0 +1,23 @@
+package com.alivehealth.alive
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.alivehealth.alive.databinding.ActivityMotiondetectBinding
+
+class MotionDetect : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_motiondetect)
+    }
+
+
+
+
+}

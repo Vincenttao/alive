fe269265da7f865be5e099d28507addef88869c9
vincenttao <13512895+vinctao@user.noreply.gitee.com>
2023-11-21 14:45:25 +0800

    去掉冗余页面代码，解决评分逻辑bug

--- app/src/main/java/com/alivehealth/alive/MotionDetectActivity.kt
+++ app/src/main/java/com/alivehealth/alive/MotionDetectActivity.kt
@@ -8,17 +8,11 @@ import android.os.Bundle
 import android.os.Process
 import android.speech.tts.TextToSpeech
 import android.view.SurfaceView
-import android.view.View
 import android.view.WindowManager
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.CompoundButton
-import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.SwitchCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.lifecycleScope
@@ -29,7 +23,6 @@ import com.alivehealth.alive.ml.MoveNet
 import com.alivehealth.alive.ml.MoveNetMultiPose
 import com.alivehealth.alive.ml.PoseClassifier
 import com.alivehealth.alive.ml.PoseNet
-import com.alivehealth.alive.ml.TrackerType
 import com.alivehealth.alive.ml.Type
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -42,9 +35,6 @@ class MotionDetectActivity : AppCompatActivity() {
     // companion object 用于声明静态成员，类似于 Java 中的 static 字段。在这个例子中，它定义了一个字符串常量。
     companion object {
         private const val FRAGMENT_DIALOG = "dialog"
-        private const val TREE_POSE_THRESHOLD = 0.5f
-        private const val TREE_POSE_DURATION = 3000L // 3秒
-        private const val TREE_POSE_EXTENDED_DURATION = 5000L
     }
 
     /** A [SurfaceView] for camera preview.   */
@@ -62,17 +52,6 @@ class MotionDetectActivity : AppCompatActivity() {
     private var device = Device.CPU
 
     // 以下各个 lateinit 变量是 UI 组件的声明，它们也会在之后被初始化。保留后续处理
-    private lateinit var tvScore: TextView
-    private lateinit var tvFPS: TextView
-    private lateinit var spnDevice: Spinner
-    private lateinit var spnModel: Spinner
-    private lateinit var spnTracker: Spinner
-    private lateinit var vTrackerOption: View
-    private lateinit var tvClassificationValue1: TextView
-    private lateinit var tvClassificationValue2: TextView
-    private lateinit var tvClassificationValue3: TextView
-    private lateinit var swClassification: SwitchCompat
-    private lateinit var vClassificationOption: View
 
     private lateinit var textToSpeech: TextToSpeech
 
@@ -87,10 +66,13 @@ class MotionDetectActivity : AppCompatActivity() {
     private var isClassifyPose = true
 
     //下面的变量用于提示用户监测结果
-    private val treePoseSmoother = MovingAverage(10)
-    private var treePoseStartTime: Long = 0
-    private var isTreePoseStandardMet = false
-    private var isTreePoseCompleted = false
+    private val poseSmoother = MovingAverage(10)
+    private var poseStartTime: Long = 0
+    private var poseLowScoreStartTime = 0L
+    private var isPoseStandardMet = false
+    private var isPoseCompleted = false
+    private var poseCount = 0
+
 
 
 
@@ -125,58 +107,6 @@ class MotionDetectActivity : AppCompatActivity() {
             }
         }
 
-    /*
-    private var changeModelListener = object : AdapterView.OnItemSelectedListener {
-        override fun onNothingSelected(parent: AdapterView<*>?) {
-            // ... 实现了 onNothingSelected 和 onItemSelected 方法，用于处理模型更改。
-        }
-
-        override fun onItemSelected(
-            parent: AdapterView<*>?,
-            view: View?,
-            position: Int,
-            id: Long
-        ) {
-            changeModel(position)
-        }
-    }
-
-    private var changeDeviceListener = object : AdapterView.OnItemSelectedListener {
-        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-            changeDevice(position)
-        }
-
-        override fun onNothingSelected(parent: AdapterView<*>?) {
-            // do nothing
-        }
-    }
-
-     */
-
-    /*
-    private var changeTrackerListener = object : AdapterView.OnItemSelectedListener {
-        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-            changeTracker(position)
-        }
-
-        override fun onNothingSelected(parent: AdapterView<*>?) {
-            // do nothing
-        }
-    }
-
-     */
-
-    /*
-    private var setClassificationListener =
-        CompoundButton.OnCheckedChangeListener { _, isChecked ->
-            showClassificationResult(isChecked)
-            isClassifyPose = isChecked
-            isPoseClassifier()
-        }
-
-     */
-
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -184,25 +114,6 @@ class MotionDetectActivity : AppCompatActivity() {
         setContentView(R.layout.activity_motiondetect)
         // keep screen on while app is running
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-
-        /*
-        tvScore = findViewById(R.id.tvScore)//将一个object对象赋值给tvScore，这样将来修改tvScore就可以修改对象的值
-        tvFPS = findViewById(R.id.tvFps)
-        spnModel = findViewById(R.id.spnModel)
-        spnDevice = findViewById(R.id.spnDevice)
-        spnTracker = findViewById(R.id.spnTracker)
-        vTrackerOption = findViewById(R.id.vTrackerOption)
-
-        tvClassificationValue1 = findViewById(R.id.tvClassificationValue1)
-        tvClassificationValue2 = findViewById(R.id.tvClassificationValue2)
-        tvClassificationValue3 = findViewById(R.id.tvClassificationValue3)
-        swClassification = findViewById(R.id.swPoseClassification)
-        vClassificationOption = findViewById(R.id.vClassificationOption)
-        initSpinner()
-        spnModel.setSelection(modelPos)
-        swClassification.setOnCheckedChangeListener(setClassificationListener)
-
-         */
 
         surfaceView = findViewById(R.id.surfaceView)
 
@@ -268,48 +179,44 @@ class MotionDetectActivity : AppCompatActivity() {
                             personScore: Float?,
                             poseLabels: List<Pair<String, Float>>?
                         ) {
-                            /*
-                            tvScore.text = getString(R.string.tfe_pe_tv_score, personScore ?: 0f)
-                            poseLabels?.sortedByDescending { it.second }?.let {
-                                tvClassificationValue1.text = getString(
-                                    R.string.tfe_pe_tv_classification_value,
-                                    convertPoseLabels(if (it.isNotEmpty()) it[0] else null)
-                                )
-                                tvClassificationValue2.text = getString(
-                                    R.string.tfe_pe_tv_classification_value,
-                                    convertPoseLabels(if (it.size >= 2) it[1] else null)
-                                )
-                                tvClassificationValue3.text = getString(
-                                    R.string.tfe_pe_tv_classification_value,
-                                    convertPoseLabels(if (it.size >= 3) it[2] else null)
-                                )
-                            }
-
-                             */
-
-                            //计算姿势的得分
+                            //计算courseCode姿势的得分
                             val poseScore = poseLabels?.find { it.first == courseCode }?.second ?: 0f
-                            treePoseSmoother.add(poseScore)
-                            val avgTreeScore = treePoseSmoother.average()
-                            val roundTreeScore = (avgTreeScore*100).roundToInt()
-                            scoreTextView.text = roundTreeScore.toString()
+                            poseSmoother.add(poseScore)
+                            val avgScore = poseSmoother.average()
+                            val roundScore = (avgScore*100).roundToInt()
+                            scoreTextView.text = roundScore.toString()
 
+
+
+                            //计数逻辑
                             val poseDuration = resources.getInteger(R.integer.pose_duration).toLong()
                             val poseExtendedDuration = resources.getInteger(R.integer.pose_extended_duration).toLong()
                             val poseThreshold = resources.getString(R.string.pose_threshold).toFloat()
 
-                            if (avgTreeScore >= poseThreshold && !isTreePoseStandardMet) {
-                                treePoseStartTime = System.currentTimeMillis()
-                                isTreePoseStandardMet = true
-                                speak(getString(R.string.tree_pose_standard_met))
-                            } else if (isTreePoseStandardMet) {
-                                val elapsedTime = System.currentTimeMillis() - treePoseStartTime
-                                if (elapsedTime >= poseDuration && !isTreePoseCompleted) {
-                                    isTreePoseCompleted = true
-                                    speak(getString(R.string.tree_pose_completed))
-                                } else if (elapsedTime < poseExtendedDuration && avgTreeScore < poseThreshold) {
-                                    isTreePoseStandardMet = false
-                                    speak(getString(R.string.tree_pose_restart))
+                            if (avgScore >= poseThreshold && !isPoseStandardMet) {
+                                poseStartTime = System.currentTimeMillis()
+                                isPoseStandardMet = true
+                                isPoseCompleted = false
+                                speak(getString(R.string.pose_standard_met))
+                            } else if (isPoseStandardMet) {
+                                val elapsedTime = System.currentTimeMillis() - poseStartTime
+                                if (elapsedTime >= poseDuration && !isPoseCompleted) {
+                                    isPoseCompleted = true
+                                    poseCount++ //姿势动作加一
+                                    speak(getString(R.string.pose_completed)+ "完成了 $poseCount 次")
+                                } else if (avgScore < poseThreshold) {
+                                    if (poseLowScoreStartTime == 0L) {
+                                        poseLowScoreStartTime = System.currentTimeMillis()
+                                    }
+
+                                    if (System.currentTimeMillis() - poseLowScoreStartTime >= poseExtendedDuration){
+                                        isPoseStandardMet = false
+                                        isPoseCompleted = false
+                                        poseLowScoreStartTime = 0L
+                                        speak(getString(R.string.pose_restart))
+                                    }
+                                }else{
+                                    poseLowScoreStartTime = 0L
                                 }
                             }
                         }
@@ -357,73 +264,6 @@ class MotionDetectActivity : AppCompatActivity() {
         cameraSource?.setClassifier(if (isClassifyPose) PoseClassifier.create(this) else null)
     }
 
-    /*
-    // Initialize spinners to let user select model/accelerator/tracker.
-    private fun initSpinner() {
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.tfe_pe_models_array,
-            android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
-            spnModel.adapter = adapter
-            spnModel.onItemSelectedListener = changeModelListener
-        }
-
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.tfe_pe_device_name, android.R.layout.simple_spinner_item
-        ).also { adaper ->
-            adaper.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
-            spnDevice.adapter = adaper
-            spnDevice.onItemSelectedListener = changeDeviceListener
-        }
-
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.tfe_pe_tracker_array, android.R.layout.simple_spinner_item
-        ).also { adaper ->
-            adaper.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
-            spnTracker.adapter = adaper
-            spnTracker.onItemSelectedListener = changeTrackerListener
-        }
-    }
-
-    // Change model when app is running
-    private fun changeModel(position: Int) {
-        if (modelPos == position) return
-        modelPos = position
-        createPoseEstimator()
-    }
-
-    // Change device (accelerator) type when app is running
-    private fun changeDevice(position: Int) {
-        val targetDevice = when (position) {
-            0 -> Device.CPU
-            1 -> Device.GPU
-            else -> Device.NNAPI
-        }
-        if (device == targetDevice) return
-        device = targetDevice
-        createPoseEstimator()
-    }
-
-    // Change tracker for Movenet MultiPose model
-    private fun changeTracker(position: Int) {
-        cameraSource?.setTracker(
-            when (position) {
-                1 -> TrackerType.BOUNDING_BOX
-                2 -> TrackerType.KEYPOINTS
-                else -> TrackerType.OFF
-            }
-        )
-    }
-
-     */
 
     /**
      * 这段代码的目的是根据条件选择合适的姿势检测器并配置其行为。最后，如果检测器和摄像头源都不为 null，
@@ -484,42 +324,6 @@ class MotionDetectActivity : AppCompatActivity() {
         }
     }
 
-    /*
-    // Show/hide the pose classification option.
-    private fun showPoseClassifier(isVisible: Boolean) {
-        vClassificationOption.visibility = if (isVisible) View.VISIBLE else View.GONE
-        if (!isVisible) {
-            swClassification.isChecked = false
-        }
-    }
-
-    // Show/hide the detection score.
-    private fun showDetectionScore(isVisible: Boolean) {
-        tvScore.visibility = if (isVisible) View.VISIBLE else View.GONE
-    }
-
-    // Show/hide classification result.
-    private fun showClassificationResult(isVisible: Boolean) {
-        val visibility = if (isVisible) View.VISIBLE else View.GONE
-        tvClassificationValue1.visibility = visibility
-        tvClassificationValue2.visibility = visibility
-        tvClassificationValue3.visibility = visibility
-    }
-
-    // Show/hide the tracking options.
-    private fun showTracker(isVisible: Boolean) {
-        if (isVisible) {
-            // Show tracker options and enable Bounding Box tracker.
-            vTrackerOption.visibility = View.VISIBLE
-            spnTracker.setSelection(1)
-        } else {
-            // Set tracker type to off and hide tracker option.
-            vTrackerOption.visibility = View.GONE
-            spnTracker.setSelection(0)
-        }
-    }
-
-     */
 
     private fun requestPermission() {
         when (PackageManager.PERMISSION_GRANTED) {
